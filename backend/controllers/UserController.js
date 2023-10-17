c9c9390c81ae2631e4d265201dc6f2446b0766e7
MurilloYonamine <picodaplantadearroz@gmail.com>
2023-10-17 11:47:12 -0300

    Criptografei as senhas passadas pela Controller do usuário.

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,4 +1,5 @@
 const User = require('../models/User');
+const bcrypt = require('bcryptjs');
 
 module.exports = class UserController {
     static async register(req, res) {
@@ -44,6 +45,24 @@ module.exports = class UserController {
             return;
         }
 
-        
+        // create a password
+        const salt = await bcrypt.genSalt(12);
+        const passwordHash = await bcrypt.hash(password, salt);
+
+        //create a user
+        const user = new User({
+            name: name,
+            email: email,
+            phone: phone,
+            password: passwordHash,
+        });
+
+        try {
+            const newUser = await user.save();
+            res.status(201).json({ message: 'Usuário criado!', newUser });
+            return
+        } catch (err) {
+            res.status(500).json({ message: error });
+        }
     }
 };
