e466d5baa4404f0230ccba920b330094a5689581
MurilloYonamine <picodaplantadearroz@gmail.com>
2023-10-17 11:32:00 -0300

    Criação da controller e route do Usuer. Além de testes no Postman;

--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,18 @@ const cors = require('cors');
 
 const app = express();
 
+// Config JSON response
 app.use(express.json());
 
+// Solve CORS
 app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
 
+//public folder for images
 app.use(express.static('public'))
+
+//Routes
+const UserRoutes = require('./routes/UserRoutes')
+
+app.use('/users', UserRoutes)
 
 app.listen(5000)
