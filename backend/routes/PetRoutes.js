8a0aca191e4ba218c0c08f639821ec259a217b2c
MurilloYonamine <picodaplantadearroz@gmail.com>
2023-10-20 21:53:02 -0300

    Criado o sistema de agendamento de Pets.

--- backend/routes/PetRoutes.js
+++ backend/routes/PetRoutes.js
@@ -16,5 +16,6 @@ router.get('/:id', PetController.getPetById);
 router.delete('/:id', verifyToken, PetController.removePetById);
 
 router.patch('/:id', verifyToken, imageUpload.array('images'), PetController.updatePet);
+router.patch('/schedule/:id', verifyToken, PetController.schedule);
 
 module.exports = router;
