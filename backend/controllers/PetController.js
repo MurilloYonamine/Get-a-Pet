8d186bb4ca2cac2c8d04244dacf7b352bc1f1851
MurilloYonamine <picodaplantadearroz@gmail.com>
2023-10-20 21:12:27 -0300

    Removendo um pet pelo seu id do sistema.

--- backend/controllers/PetController.js
+++ backend/controllers/PetController.js
@@ -99,6 +99,7 @@ module.exports = class PetController {
     static async getPetById(req, res) {
         const id = req.params.id;
 
+        // check if Id is valid
         if (!ObjectId.isValid(id)) {
             res.status(422).json({ message: "Id inválido!" });
             return;
@@ -114,5 +115,34 @@ module.exports = class PetController {
         res.status(200).json({
             pet: pet,
         });
+    }
+    static async removePetById(req, res) {
+        const id = req.params.id;
+
+        // check if Id is valid
+        if (!ObjectId.isValid(id)) {
+            res.status(422).json({ message: "Id inválido!" });
+            return;
+        }
+
+        // check if pets exists
+        const pet = await Pet.findOne({ _id: id });
+
+        if (!pet) {
+            res.status(404).json({ message: 'Pet não encontrado!' });
+            return;
+        }
+
+        // check if logged in user registered the pet
+        const token = getToken(req);
+        const user = await getUserByToken(token);
+
+        if (pet.user._id.toString() !== user._id.toString()) {
+            res.status(422).json({ message: "Houve um problema em processar a sua socilitação! Tente novamente mais tarde!" });
+            return;
+        }
+
+        await Pet.findByIdAndRemove(id);
+        res.status(200).json({ message: 'Pet removido com sucesso!' });
     }
 };
