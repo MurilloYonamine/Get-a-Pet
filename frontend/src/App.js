9d2edd04156df6357e7645cc8d23aa81c69fa7b7
MurilloYonamine <picodaplantadearroz@gmail.com>
2023-10-23 14:27:55 -0300

    Estruturação e CSS do footer e da Navbar.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+/* Components */
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+
 /* Pages */
 import Login from './components/pages/Auth/Login';
 import Register from './components/pages/Auth/Register';
@@ -10,17 +14,13 @@ class App extends Component {
   render() {
     return (
       <Router>
+        <Navbar />
         <Routes>
-          <Route path='/login'>
-            <Login />
-          </Route>
-          <Route path='/register'>
-            <Register />
-          </Route>
-          <Route path='/'>
-            <Home />
-          </Route>
+          <Route path='/' exact element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
         </Routes>
+        <Footer />
       </Router>
     );
   }
--- frontend/src/components/pages/Auth/Register.js
+++ frontend/src/components/pages/Auth/Register.js
@@ -1,5 +1,3 @@
-import React, { Component } from 'react';
-
 function Register() {
     return (
         <section>
