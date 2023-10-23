61c0b1b4393966d9cd94e276ee727a74e4728e71
MurilloYonamine <picodaplantadearroz@gmail.com>
2023-10-23 17:59:06 -0300

    Criado o formulário de registro.

--- frontend/src/components/pages/Auth/Register.js
+++ frontend/src/components/pages/Auth/Register.js
@@ -1,7 +1,21 @@
+import Input from '../../form/input';
+
 function Register() {
+
+    function handleChange(e) { }
+
     return (
         <section>
-            <h1>Register</h1>
+            <h1>Registar</h1>
+            <form>
+                <Input text="Nome" type="text" name="name" placeholder="Digite o seu nome" handleOnChange={handleChange} />
+                <Input text="Telefone" type="text" name="phone" placeholder="Digite o seu telefone" handleOnChange={handleChange} />
+                <Input text="E-mail" type="email" name="email" placeholder="Digite o seu e-mail" handleOnChange={handleChange} />
+                <Input text="Senha" type="password" name="senha" placeholder="Digite a sua senha" handleOnChange={handleChange} />
+                <Input text="Confirmação de senha" type="password" name="senha" placeholder="Confirme a sua senha" handleOnChange={handleChange} />
+
+                <input type="submit" value="Cadastrar" />
+            </form>
         </section>
     );
 }
