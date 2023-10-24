3d5d7c6331e3197ab289ac5e9c23f4ebb37b4f4c
MurilloYonamine <picodaplantadearroz@gmail.com>
2023-10-24 09:58:50 -0300

    Finalizado as flash messages.

--- frontend/src/components/pages/Auth/Register.js
+++ frontend/src/components/pages/Auth/Register.js
@@ -26,10 +26,10 @@ function Register() {
             <h1>Registar</h1>
             <form onSubmit={handleSubmit}>
                 <Input text="Nome" type="text" name="name" placeholder="Digite o seu nome" handleOnChange={handleChange} />
-                <Input text="Telefone" type="text" name="phone" placeholder="Digite o seu telefone" handleOnChange={handleChange} />
                 <Input text="E-mail" type="email" name="email" placeholder="Digite o seu e-mail" handleOnChange={handleChange} />
-                <Input text="Senha" type="password" name="senha" placeholder="Digite a sua senha" handleOnChange={handleChange} />
-                <Input text="Confirmação de senha" type="password" name="confirmarSenha" placeholder="Confirme a sua senha" handleOnChange={handleChange} />
+                <Input text="Telefone" type="text" name="phone" placeholder="Digite o seu telefone" handleOnChange={handleChange} />
+                <Input text="Senha" type="password" name="password" placeholder="Digite a sua senha" handleOnChange={handleChange} />
+                <Input text="Confirmação de senha" type="password" name="confirmpassword" placeholder="Confirme a sua senha" handleOnChange={handleChange} />
 
                 <input type="submit" value="Cadastrar" />
             </form>
