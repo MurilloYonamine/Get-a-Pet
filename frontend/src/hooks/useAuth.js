c03a5b761ab12108e016063c4388a492d02a1df1
MurilloYonamine <picodaplantadearroz@gmail.com>
2023-10-25 20:21:30 -0300

    Autenticação do usuário cadastrado pela metade.

--- frontend/src/hooks/useAuth.js
+++ frontend/src/hooks/useAuth.js
@@ -1,15 +1,15 @@
 import api from '../utils/api';
 
 import { useState, useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import useFlashMessage from './useFlashMessage';
 
 export default function useAuth() {
-
+    const { authenticated, setAuthenticated } = useState(false);
     const { setFlashMessage } = useFlashMessage();
+    const navigate = useNavigate();
 
     async function register(user) {
-
         let msgText = 'Cadastro realizado com sucesso!';
         let msgType = 'success';
 
@@ -17,16 +17,20 @@ export default function useAuth() {
             const data = await api.post('/users/register', user).then((response) => {
                 return response.data;
             });
-
             console.log(data);
+            await authUser(data);
         } catch (error) {
-            // tratar erro
             msgText = error.response.data.message;
             msgType = 'error';
         }
 
         setFlashMessage(msgText, msgType);
+    }
+    async function authUser(data) {
+        setAuthenticated(true);
+        localStorage.setItem('token', JSON.stringify(data.token));
 
+        navigate('/');
     }
     return { register };
 }
